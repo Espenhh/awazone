ccc5eaad2d94390f11cd40a89ccc8113726090f2
Kjetil Valle <kjetil.valle@gmail.com>
2013-01-22 23:57:12 +0100

    gjort ferdig deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
-function yellow {
-	echo -e "\033[33m$1\033[0m"
-}
+yellow() { echo -e "\033[33m$1\033[0m"; }
+red() { echo -e "\033[31m$1\033[0m"; }
+bold() { echo -e "\033[1;37m$1\033[0m"; }
 
-function bold {
-	echo -e "\033[1;37m$1\033[0m"
-}
+info() { bold "$1"; }
+fail() { red "$1"; exit 1; }
 
 yellow ""
 yellow "  deployer                                   "
@@ -18,11 +18,6 @@ yellow "      #    #             ######  #####      #"
 yellow " #    #   #              #    #  #          #"
 yellow "  ####   ######          #    #  #          #"
 yellow ""
-
-function fail {
-	echo "\[${bold_green}\]$1\[${reset}\]"
-	exit 1
-}
 
 DEFAULT_JAR=`find . | grep dependencies.jar`
 DEFAULT_VERSION=`date +%Y%m%d%H%M%S`-SNAPSHOT
@@ -39,9 +34,13 @@ if [ $ENV != "test" -a $ENV != "prod" ]; then
 	fail "Miljø må være enten 'test' eller 'prod'"
 fi
 
+info "starter deploy"
+
 BASE="~/web/api-app/$ENV"
 ssh javabin@javazone.espenhh.com "mkdir -p $BASE/$VERSION"
 scp $JAR javabin@javazone.espenhh.com:$BASE/$VERSION/awazone.jar
 ssh javabin@javazone.espenhh.com "ln -s -f $VERSION -T $BASE/current"
+ssh javabin@javazone.espenhh.com "$BASE/app.sh stop"
+ssh javabin@javazone.espenhh.com "$BASE/app.sh start"
 
 yellow "le done"
