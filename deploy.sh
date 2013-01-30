64fe04c1642a79fc2b9e8e5368e621375e1cd689
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2013-01-30 19:00:56 +0100

    Fikser deployscript fordi teite OSX ikke kan bruke -i på read...

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
 yellow() { echo -e "\033[33m$1\033[0m"; }
+green() { echo -e "\033[32m$1\033[0m"; }
 red() { echo -e "\033[31m$1\033[0m"; }
 bold() { echo -e "\033[1;37m$1\033[0m"; }
+_fancymessage() {
+  echo ""
+  green "\033[32m--> $1 \033[0m"
+}
 
 info() { bold "$1"; }
+ask() { _fancymessage "$1"; }
 fail() { red "$1"; exit 1; }
+
+# read -i funker ikke på OSX - derav mer kronglete løsning :(
+_readWithDefault() {
+    local default=$1
+    read answer
+    if [ "$answer" = "" ]; then
+         answer="$default"
+    fi
+    echo $answer
+}
+
+
 
 yellow ""
 yellow "  deployer                                   "
@@ -22,9 +40,14 @@ yellow ""
 DEFAULT_JAR=`find . | grep dependencies.jar`
 DEFAULT_VERSION=`date +%Y%m%d%H%M%S`-SNAPSHOT
 
-read -e -p "> Hvor ligger jar-filen? " -i $DEFAULT_JAR JAR
-read -e -p "> Hvilken versjon? " -i $DEFAULT_VERSION VERSION
-read -e -p "> Til test eller prod? " -i "test" ENV
+ask "Hvor ligger jar-filen? [$DEFAULT_JAR]"
+JAR=$(_readWithDefault $DEFAULT_JAR)
+
+ask "Hvilken versjon? [$DEFAULT_VERSION]"
+VERSION=$(_readWithDefault $DEFAULT_VERSION)
+
+ask "Til test eller prod? [test]"
+ENV=$(_readWithDefault "test")
 
 if [ ! -f $JAR ]; then
 	fail "Fant ikke $JAR :("
@@ -34,7 +57,7 @@ if [ $ENV != "test" -a $ENV != "prod" ]; then
 	fail "Miljø må være enten 'test' eller 'prod'"
 fi
 
-info "starter deploy"
+info "Deployer til $ENV med versjon $VERSION med jar $JAR"
 
 BASE="~/web/api-app/$ENV"
 ssh javabin@javazone.espenhh.com "mkdir -p $BASE/$VERSION"
