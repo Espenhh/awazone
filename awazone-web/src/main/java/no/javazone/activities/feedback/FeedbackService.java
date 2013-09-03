4212d2b0bccb73fa124e726d782d689956589ea6
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2013-09-03 17:21:57 +0200

    Feedback kan nå mangle rating eller comment

--- awazone-web/src/main/java/no/javazone/activities/feedback/FeedbackService.java
+++ awazone-web/src/main/java/no/javazone/activities/feedback/FeedbackService.java
@@ -1,22 +1,21 @@
 package no.javazone.activities.feedback;
 
-import static com.google.common.collect.Collections2.transform;
-import static com.google.common.collect.Lists.newArrayList;
-
 import java.net.UnknownHostException;
 import java.util.List;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import no.javazone.representations.feedback.Feedback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Function;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 public class FeedbackService {
@@ -44,30 +43,18 @@ public class FeedbackService {
 
 		try {
 			System.out.println("Fant " + cursor.size() + " feedbacks som matcher talkid " + talkId);
-			List<DBObject> feedbacksFromDb = cursor.toArray();
-
-			List<Feedback> feedbacks = newArrayList(transform(feedbacksFromDb, new Function<DBObject, Feedback>() {
-				@Override
-				public Feedback apply(final DBObject dbObject) {
-					return new Feedback(dbObject.get("_id").toString(), dbObject.get("talkId").toString(), Integer.parseInt(dbObject.get(
-							"rating").toString()), dbObject.get("comment").toString());
-				}
-			}));
-
-			return feedbacks;
-
+			return Feedback.convertFromMongo(cursor.toArray());
 		} finally {
 			cursor.close();
 		}
 	}
 
 	public void addFeedbackForTalk(final String talkId, final Feedback feedback) {
-		BasicDBObject object = new BasicDBObject()
-				.append("talkId", talkId)
-				.append("rating", feedback.rating)
-				.append("comment", feedback.comment);
-		collection.insert(object);
-		System.out.println("La til objekt. ID=" + object.get("_id"));
+		if (!feedback.validate()) {
+			throw new WebApplicationException(Response.status(Status.BAD_REQUEST).entity("Ikke gyldig feedback!").build());
+		}
+
+		collection.insert(feedback.toMongoObject(talkId));
 	}
 
 	public static FeedbackService getInstance() {
--- awazone-web/src/main/java/no/javazone/representations/feedback/Feedback.java
+++ awazone-web/src/main/java/no/javazone/representations/feedback/Feedback.java
@@ -1,14 +1,20 @@
 package no.javazone.representations.feedback;
 
+import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
+import com.google.common.base.Function;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
 public class Feedback {
 
-	@JsonProperty
-	public final String feedbackId;
-	@JsonProperty
-	public final String talkId;
 	@JsonProperty
 	public final Integer rating;
 	@JsonProperty
@@ -16,17 +22,34 @@ public class Feedback {
 
 	@JsonCreator
 	public Feedback(@JsonProperty("rating") final Integer rating, @JsonProperty("comment") final String comment) {
-		feedbackId = null;
-		talkId = null;
 		this.rating = rating;
 		this.comment = comment;
 	}
 
-	public Feedback(final String feedbackId, final String talkId, final Integer rating, final String comment) {
-		this.feedbackId = feedbackId;
-		this.talkId = talkId;
-		this.rating = rating;
-		this.comment = comment;
+	public boolean validate() {
+		return rating > 1 && rating < 4;
+	}
+
+	public DBObject toMongoObject(final String talkId) {
+		return new BasicDBObject().append("talkId", talkId).append("rating", rating).append("comment", comment);
+	}
+
+	public static ArrayList<Feedback> convertFromMongo(final List<DBObject> feedbacksFromDb) {
+		return newArrayList(transform(feedbacksFromDb, new Function<DBObject, Feedback>() {
+			@Override
+			public Feedback apply(final DBObject dbObject) {
+				Integer rating;
+				try {
+					rating = Integer.parseInt(dbObject.get("rating").toString());
+				} catch (NumberFormatException e) {
+					rating = null;
+				}
+
+				Object commentObj = dbObject.get("comment");
+				String comment = commentObj != null ? commentObj.toString() : null;
+				return new Feedback(rating, comment);
+			}
+		}));
 	}
 
 }
