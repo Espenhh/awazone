69a71f47df44378acb477d0efb6e0988dda32cf4
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2013-09-07 22:05:07 +0200

    Sørger for at adminressurser ikke kan kalles uten videre...

--- awazone-web/src/main/java/no/javazone/api/resources/admin/AdminFeedbackResource.java
+++ awazone-web/src/main/java/no/javazone/api/resources/admin/AdminFeedbackResource.java
@@ -6,14 +6,18 @@ import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import no.javazone.activities.feedback.FeedbackService;
 import no.javazone.api.filters.NoCacheResponseFilter;
 import no.javazone.representations.feedback.Feedback;
+import no.javazone.server.PropertiesLoader;
 
 import com.sun.jersey.spi.container.ResourceFilters;
 
@@ -25,7 +29,8 @@ public class AdminFeedbackResource {
 
 	@GET
 	@Produces(APPLICATION_JSON)
-	public Response getAllFeedbacks() {
+	public Response getAllFeedbacks(@HeaderParam("secret") final String secret) {
+		checkSecret(secret);
 		Map<String, List<Feedback>> feedbacks = feedbackService.getAllFeedbacks();
 		return Response.ok(feedbacks).build();
 	}
@@ -33,7 +38,8 @@ public class AdminFeedbackResource {
 	@GET
 	@Path("/{talkId}")
 	@Produces(APPLICATION_JSON)
-	public Response getFeedbacksFor(@PathParam("talkId") final String talkId) {
+	public Response getFeedbacksFor(@HeaderParam("secret") final String secret, @PathParam("talkId") final String talkId) {
+		checkSecret(secret);
 		List<Feedback> feedbacks = feedbackService.getFeedbacksForTalk(talkId);
 		return Response.ok(feedbacks).build();
 	}
@@ -41,9 +47,16 @@ public class AdminFeedbackResource {
 	@GET
 	@Path("/{talkId}/summary")
 	@Produces(APPLICATION_JSON)
-	public Response getSummaryFor(@PathParam("talkId") final String talkId) {
+	public Response getSummaryFor(@HeaderParam("secret") final String secret, @PathParam("talkId") final String talkId) {
+		checkSecret(secret);
 		String s = feedbackService.getSummaryForTalk(talkId);
 		return Response.ok(s).build();
+	}
+
+	private void checkSecret(final String secret) {
+		if (secret == null || !secret.equals(PropertiesLoader.getProperty("api.secret"))) {
+			throw new WebApplicationException(Status.FORBIDDEN);
+		}
 	}
 
 }
--- awazone-web/src/main/java/no/javazone/api/resources/admin/AdminSessionsResource.java
+++ awazone-web/src/main/java/no/javazone/api/resources/admin/AdminSessionsResource.java
@@ -5,13 +5,17 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import no.javazone.activities.ems.SessionsActivity;
 import no.javazone.api.filters.NoCacheResponseFilter;
 import no.javazone.representations.sessions.AdminSimpleSession;
+import no.javazone.server.PropertiesLoader;
 
 import com.sun.jersey.spi.container.ResourceFilters;
 
@@ -23,9 +27,16 @@ public class AdminSessionsResource {
 
 	@GET
 	@Produces(APPLICATION_JSON)
-	public Response getSimpleAdminSessions() {
+	public Response getSimpleAdminSessions(@HeaderParam("secret") final String secret) {
+		checkSecret(secret);
 		List<AdminSimpleSession> simpleSessions = sessionsActivity.getSimpleSessionsAdmin();
 		return Response.ok(simpleSessions).build();
+	}
+
+	private void checkSecret(final String secret) {
+		if (secret == null || !secret.equals(PropertiesLoader.getProperty("api.secret"))) {
+			throw new WebApplicationException(Status.FORBIDDEN);
+		}
 	}
 
 }
